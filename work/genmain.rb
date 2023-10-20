e5db6cdb4e36e2a9d1c236de330d4c485ebca84b
Unno Hideyuki <unno.hideyuki@gmail.com>
2023-10-21 01:50:44 +0900

    first demo program of genmain.rb and UnionFind

---
+++ work/genmain.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+
+
+LIBDIR = "../../lib/src/main/java/jp/ne/sakura/uhideyuki/jatcoder"
+
+LIB_CLASSES =
+  Dir.glob("#{LIBDIR}/*.java").map{|s| File.basename(s, ".java")}
+
+template = <<'EOS'
+import java.util.*;
+
+public class Main {
+    public static void main(final String[] args){
+        <%= clsname %>.solve();
+    }
+}
+
+EOS
+
+## Read the source from stdin
+
+instr = $stdin.read
+instr.gsub(/^package.*$/, "")
+
+## Detect used libraries
+
+libs = []
+LIB_CLASSES.each{|s|
+  re = Regexp.new("#{s}[\.\:\(]")
+  if re =~ instr
+    libs.append(s)
+  end
+}
+
+## Print Main
+re1 = /^\s*public\s+class\s+(\w+)/
+re1 =~ instr
+clsname = $1
+erb = ERB.new(template)
+erb.run
+
+## Insert source
+re2 = /^\s*(import|package).*$/
+instr = instr.gsub(re2, "");
+instr = instr.gsub(re1, "class #{clsname}")
+puts instr
+
+## Insert libs
+libs.each{|s|
+  File.open("#{LIBDIR}/#{s}.java") do |f|
+    str = f.read
+    str = str.gsub(re1, "class #{s}")
+    str = str.gsub(re2, "")
+    puts str
+  end
+}
+
+
+  
+
+  
---
+++ work/practice2a/Practice2A.java
@@ -0,0 +1,24 @@
+package jp.ne.sakura.uhideyuki.jatcoder;
+
+import java.util.*;
+
+public class Practice2A {
+    public static void solve(){
+        Scanner sc = new Scanner(System.in);
+        final int N = sc.nextInt();
+        final int Q = sc.nextInt();
+
+        UnionFind uf = new UnionFind(N);
+
+        for (int i = 0; i < Q; i++){
+            int t = sc.nextInt();
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            if (t == 0){
+                uf.merge(u, v);
+            } else {
+                System.out.println(uf.same(u, v) ? 1 : 0);
+            }
+        }
+    }
+}
