ca7a9809048112c41b1eee7764cc212029f8f714
Unno Hideyuki <unno.hideyuki@gmail.com>
2023-10-24 00:13:23 +0900

    modified to add @Override

--- lib/src/main/java/jp/ne/sakura/uhideyuki/jatcoder/Pair.java
+++ lib/src/main/java/jp/ne/sakura/uhideyuki/jatcoder/Pair.java
@@ -1,6 +1,5 @@
 package jp.ne.sakura.uhideyuki.jatcoder;
-public class Pair<L extends Comparable<L>, R extends Comparable<R>>
-        implements Comparable<Pair<L, R>>{
+public class Pair<L extends Comparable<L>, R extends Comparable<R>> implements Comparable<Pair<L, R>> {
     public L first;
     public R second;
     public Pair(final L a, final R b){
@@ -8,14 +7,23 @@ public class Pair<L extends Comparable<L>, R extends Comparable<R>>
         second = b;
     }
 
-    public boolean equals(final Pair<L, R> p){
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean equals(final Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        final Pair<L, R> p = (Pair<L, R>) obj;
         return first == p.first && second == p.second;
     }
 
+    @Override
     public int compareTo(final Pair<L, R> p){
         return (first != p.first) ? first.compareTo(p.first) : second.compareTo(p.second);
     }
 
+    @Override
     public int hashCode(){
         return first.hashCode() ^ second.hashCode();
     }
