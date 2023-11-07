9f6e7ce25edab446cdbbde0ef2fd630c8ecfa315
Unno Hideyuki <unno.hideyuki@gmail.com>
2023-11-07 23:47:57 +0900

    FastScanner

---
+++ lib/src/main/java/jp/ne/sakura/uhideyuki/jatcoder/FastScanner.java
@@ -0,0 +1,9 @@
+package jp.ne.sakura.uhideyuki.jatcoder;
+import java.util.*;
+public class FastScanner {
+    private final Scanner sc = new Scanner(System.in);
+    public String next() { return sc.next(); }
+    public int nextInt() { return Integer.parseInt(sc.next()); }
+    public long nextLong() { return Long.parseLong(sc.next()); }
+    public double nextDouble() { return Double.parseDouble(sc.next()); }
+}
